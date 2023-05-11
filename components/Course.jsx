fbe9192f19524f0d2261458d8dbf53bbc50e7e2b
Nahuel Chamorro <nahu.chamo19@gmail.com>
2023-05-10 23:21:06 -0300

    feature/View-mycurses[id]
    created and added Vies Component myCourses[id] , and changre routes of componentes Courses and Courses [id] for myCourses and myCourses[id]

--- components/Course.jsx
+++ components/Course.jsx
@@ -1,27 +1,27 @@
 import Image from "next/image";
 
 
-export default function Course({ img, title, tema, duration, description, imgAuthor, author, price, offer, id }) {
+export default function Course({img,title,tema,duration,description,imgAuthor,author,price,offer,id}){
     return (
         <article className="pt-4 px-4 bg-[white] pb-6 rounded-[1.25rem] shadow-2xl" key={id}>
-            <Image className="rounded-[1.25rem] h-[200px] object-cover w-[100%]" src={img} alt={title} width={300}
-                height={300} />
-            <div className="flex justify-between my-[1rem]">
-                <span className="font-medium text-[0.875rem] leading-[1.563rem] text-[#696984]">{tema}</span>
-                <span className="font-medium text-[0.875rem] leading-[1.563rem] text-[#696984]">{duration} Month</span>
-            </div>
-            <h3 className="font-medium text-[1.3rem] leading-[1.9rem] text-[#252641] mb-[1.2rem]">{title}</h3>
-            <p className="font-normal text-[1rem] leading-[1.6rem] text-[#696984] mb-[1.063rem]">{description}</p>
-            <div className="flex items-end justify-between">
-                <div className="flex items-center">
-                    <Image className="inline  bg-slate-500 h-[35px] w-[35px] object-cover object-top rounded-full" src={imgAuthor} alt={author} width={500} height={100} />
-                    <span className="font-medium text-[1rem] leading-[1.688rem] ml-[0.938rem]">{author}</span>
-                </div>
-                <div>
-                    <span className="font-light italic line-through text-[1rem] leading-[1.688rem] mr-4 text-[#00000080]">${price}</span>
-                    <span className="font-bold text-[1.3rem] leading-[2.25rem] text-[#222129] ">${offer}</span>
-                </div>
-            </div>
-        </article>
+                                <Image className="rounded-[1.25rem] h-[200px] object-cover w-[100%]" src={img} alt={title} width={300}
+                                    height={300} />
+                                <div className="flex justify-between my-[1rem]">
+                                    <span className="font-medium text-[0.875rem] leading-[1.563rem] text-[#696984]">{tema}</span>
+                                    <span className="font-medium text-[0.875rem] leading-[1.563rem] text-[#696984]">{duration} Month</span>
+                                </div>
+                                <h3 className="font-medium text-[1.3rem] leading-[1.9rem] text-[#252641] mb-[1.2rem]">{title}</h3>
+                                <p className="font-normal text-[1rem] leading-[1.6rem] text-[#696984] mb-[1.063rem]">{description}</p>
+                                <div className="flex items-end justify-between">
+                                    <div className="flex items-center">
+                                        <Image className="inline  bg-slate-500 h-[35px] w-[35px] object-cover object-top rounded-full" src={imgAuthor} alt={author} width={500} height={100} />
+                                        <span className="font-medium text-[1rem] leading-[1.688rem] ml-[0.938rem]">{author}</span>
+                                    </div>
+                                    <div>
+                                        <span className="font-light italic line-through text-[1rem] leading-[1.688rem] mr-4 text-[#00000080]">${price}</span>
+                                        <span className="font-bold text-[1.3rem] leading-[2.25rem] text-[#222129] ">${offer}</span>
+                                    </div>
+                                </div>
+                            </article>
     )
 }
--- components/SubSection.jsx
+++ components/SubSection.jsx
@@ -1,21 +1,77 @@
-export default function SubSection(){
+import cert from '../public/cert-ico.png'
+import Image from 'next/image'
+
+
+export default function SubSection({ title }) {
     return (
-            <div>
-                <h3>This Course included</h3>
-                <div>
-                    <div>
-                        
+        <>
+            {
+                title === "This Course included"
+                    ?
+                    <div className="mb-[2.1rem] pt-[2rem] border-t-2 border-[#696984]">
+                        <h3 className='font-semibold text-[1.875rem] text-start leading-[2.819rem] mb-[2rem]'>{title}</h3>
+                        <div className='flex flex-col gap-y-[0.6rem]'>
+                            <div className='flex  items-center gap-x-[0.5rem]'>
+                                <Image className='w-[20px] h-[20px]' src={cert} alt='cert-ico' width={50} height={50} />
+                                <span className='font-semibold text-[0.875rem] leading-[1.313] text-[#00000080]'>Money Back Guarantee</span>
+                            </div>
+                            <div className='flex  items-center gap-x-[0.5rem]'>
+                                <Image className='w-[20px] h-[20px]' src={cert} alt='cert-ico' width={50} height={50} />
+                                <span className='font-semibold text-[0.875rem] leading-[1.313] text-[#00000080]'>Access on all devices</span>
+                            </div>
+                            <div className='flex  items-center gap-x-[0.5rem]'>
+                                <Image className='w-[20px] h-[20px]' src={cert} alt='cert-ico' width={50} height={50} />
+                                <span className='font-semibold text-[0.875rem] leading-[1.313] text-[#00000080]'>Certification of completion</span>
+                            </div>
+                            <div className='flex  items-center gap-x-[0.5rem]'>
+                                <Image className='w-[20px] h-[20px]' src={cert} alt='cert-ico' width={50} height={50} />
+                                <span className='font-semibold text-[0.875rem] leading-[1.313] text-[#00000080]'>Moduls</span>
+                            </div>
+                        </div>
                     </div>
-                    <div>
 
-                    </div>
-                    <div>
+                    : <></>
 
-                    </div>
-                    <div>
+            }
 
-                    </div>
+
+            {/* {------------------------------------------------------------------} */}
+
+
+            {title === "Training 5 or more people"
+                ?
+
+                <div className="mb-[2.1rem] pt-[2rem] border-t-2 border-[#696984]">
+                    <h3 className='font-semibold text-[1.875rem] text-start leading-[2.819rem] mb-[2rem]'>{title}</h3>
+                    <p className='font-normal text-[0.875rem] leading-[1.575rem] text-[#696984] text-start'>Class, launched less than a year ago by Blackboard co-founder Michael Chasen, integrates exclusively...</p>
                 </div>
-            </div>
+
+                : <></>
+            }
+
+            {/* {------------------------------------------------------------------------} */}
+
+            {
+                title === "Share this course"
+                    ?
+                    <div className="mb-[2.1rem] pt-[2rem] border-t-2 border-[#696984]">
+
+                        <h3 className='font-semibold text-[1.875rem] text-start leading-[2.819rem] mb-[2rem]'>{title}</h3>
+                        <div className='flex justify- gap-x-4'>
+                            <Image className='w-[32px] h-[32px]' src={cert} alt='cert-ico' width={50} height={50} />
+                            <Image className='w-[32px] h-[32px]' src={cert} alt='cert-ico' width={50} height={50} />
+                            <Image className='w-[32px] h-[32px]' src={cert} alt='cert-ico' width={50} height={50} />
+                            <Image className='w-[32px] h-[32px]' src={cert} alt='cert-ico' width={50} height={50} />
+                            <Image className='w-[32px] h-[32px]' src={cert} alt='cert-ico' width={50} height={50} />
+                            <Image className='w-[32px] h-[32px]' src={cert} alt='cert-ico' width={50} height={50} />
+                        </div>
+
+                    </div>
+
+                    : <></>
+            }
+
+        </>
     )
+
 }
