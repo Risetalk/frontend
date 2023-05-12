1021562e2a89e9e2d18a9fbf1a0e4fe2a0271106
elizabeth ortiz <elizabeth-ortiz-rivas@hotmail.es>
2023-05-12 11:56:14 -0500

    Fixed issu Component
    Changes.

--- components/ContainerCards.jsx
+++ components/ContainerCards.jsx
@@ -1,57 +1,53 @@
 'use client'
 
-import Link from "next/link"
-import Course from "./Course"
-import { useEffect, useState } from "react"
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import Course from "./Course";
 
+export default function ContainerCards({ title, link }) {
+  const [recommends, setRecommends] = useState([]);
 
-export default async function ContainerCards({ title, link }) {
-    
-    const [recommends,setRecommends] = useState([]);
-    
-    const  getRecommend = async () =>{
-        const res = await fetch("http://localhost:3000/recommend.json")
-        const data =await res.json()
-        setRecommends(data)
-        
-    }
+  useEffect(() => {
+    const getRecommend = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/recommend.json");
+        const data = await res.json();
+        setRecommends(data);
+      } catch (error) {
+        console.error("Error fetching recommends:", error);
+      }
+    };
 
-    useEffect(()=>{
+    getRecommend();
+  }, []);
 
+  return (
+    <section className="w-[87%] m-[auto]">
+      <div className="flex justify-between w-[100%] items-center mb-[2.7rem]">
+        <h2 className="inline font-medium text-[1.65rem] leading-[2.813rem] text-[black]">
+          {title}
+        </h2>
+        <Link href={`#`}>
+          <span className="font-bold text-[1rem] text-[#222129]">{link}</span>
+        </Link>
+      </div>
 
-        getRecommend()
-        
-    },[])
-    
-    console.log(recommends);
-    
-    return (
-        <section className="w-[87%]  m-[auto] ">
-            <div className="flex justify-between w-[100%] items-center mb-[2.7rem]">
-                <h2 className="inline font-medium text-[1.65rem] leading-[2.813rem] text-[black]">{title}</h2>
-                <Link href={`#`}><span className="font-bold text-[1rem] text-[#222129]">{link}</span>
-                </Link>
-            </div>
-
-            <div className="flex gap-x-[2.9rem] text-[black]">
-                {
-                    recommends?.map(course => {
-                        return (
-                            <Course key={course.id}
-                                img={course.img}
-                                title={course.title}
-                                tema={course.tema}
-                                duration={course.duration}
-                                description={course.description}
-                                imgAuthor={course.imgAuthor}
-                                author={course.author}
-                                price={course.price}
-                                offer={course.offer}
-                            />
-                        )
-                    })
-                }
-            </div>
-        </section>
-    )
+      <div className="flex gap-x-[2.9rem] text-[white]">
+        {recommends.map((course) => (
+          <Course
+            key={course.id}
+            img={course.img}
+            title={course.title}
+            tema={course.tema}
+            duration={course.duration}
+            description={course.description}
+            imgAuthor={course.imgAuthor}
+            author={course.author}
+            price={course.price}
+            offer={course.offer}
+          />
+        ))}
+      </div>
+    </section>
+  );
 }
