314a484de1a2888161b74f774f348764f04b9bb3
elizabeth ortiz <elizabeth-ortiz-rivas@hotmail.es>
2023-05-14 13:18:12 -0500

    Feature/Modification Component
    in this commit
    -Add slice to the component.

--- components/SearchCourses.jsx
+++ components/SearchCourses.jsx
@@ -1,28 +1,26 @@
 import CourseCard from "./CourseCard";
 import { useSelector } from "react-redux";
 
-
 export default function SearchCourses() {
-  const courses = useSelector(state => state.courses)
+  const courses = useSelector((state) => state.courses);
+
+  const slicedCourses = courses?.filterCourses?.slice(0, 4);
 
   return (
     <div className="justify-between my-[3rem] mr-[8rem] ml-[6rem] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
-      {
-        courses?.filterCourses?.map((course, index) => (
-          <CourseCard key={index}
-            image={course?.image}
-            title={course?.title}
-            tema={course?.tema}
-            duration={course?.duration}
-            description={course?.description}
-            author={course?.author}
-            price={course?.price}
-            offer={course?.offer}
-          />
-        ))
-
-
-      }
+      {slicedCourses?.map((course, index) => (
+        <CourseCard
+          key={index}
+          image={course?.image}
+          title={course?.title}
+          tema={course?.tema}
+          duration={course?.duration}
+          description={course?.description}
+          author={course?.author}
+          price={course?.price}
+          offer={course?.offer}
+        />
+      ))}
     </div>
   );
 }
