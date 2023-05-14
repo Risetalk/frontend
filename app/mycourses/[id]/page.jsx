709ee38c2d7cc04554c6c06359309dfcfbe1db4b
elizabeth ortiz <elizabeth-ortiz-rivas@hotmail.es>
2023-05-14 11:12:05 -0500

    Feature/FullcourseComponent
    In this commit
    -new component view added

--- app/mycourses/[id]/page.jsx
+++ app/mycourses/[id]/page.jsx
@@ -1,8 +1,149 @@
+"use client";
+import React, { useState } from "react";
+import ContainerCards from "../../../components/ContainerCards";
+import Image from "next/image";
+import Link from "next/link";
 
+const CourseDetail = () => {
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const [additionalText, setAdditionalText] = useState("");
+  const [additionalTittle, setAdditionalTittle] = useState("");
 
-export default function CourseDetail() {
-    return (
-        <>
-        </>
-    )
-}
+  const videos = [
+    {
+      id: 1,
+      title: "Lesson 1: Introduction to ReactJs",
+      url: "https://www.youtube.com/embed/T_j60n1zgu0",
+      time: "1:30",
+      additionalText: "Additional text for video 1",
+      additionalTittle: "O6 Super Coins on the way",
+    },
+    {
+      id: 2,
+      title: "Lesson 2: React Components",
+      url: "https://www.youtube.com/embed/SQbZ9dThB80",
+      time: "2:15",
+      additionalText: "Additional text for video 2",
+      additionalTittle: "Who this course is for?",
+    },
+    {
+      id: 3,
+      title: "Lesson 3: React Hooks",
+      url: "https://www.youtube.com/embed/2NDLa2pNoh0",
+      time: "1:45",
+      additionalText:
+        "Lorem ipsum dolor sit amet, consectetur adi piscing elit, sed do eiusmodadipiscing elit, sed do eiusmodLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmodadipiscing elit, sed do eiusmodLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmodadipiscing elit, sed do eiusmodeiusmodadipiscing elit, sed do eiusmod",
+      additionalTittle: "Archievable",
+    },
+    {
+      id: 4,
+      title: "React-redux",
+      url: "https://www.youtube.com/embed/Ul1bjSY0J3w",
+      time: "1:30",
+      additionalText: "Additional text for video 1",
+      additionalTittle: "O6 Super Coins on the way",
+    },
+    {
+      id: 5,
+      title: "React Hooks",
+      url: "https://www.youtube.com/embed/fQoDUj46pwU?controls=0",
+      time: "1:30",
+      additionalText: "Additional text for video 1",
+      additionalTittle: "O6 Super Coins on the way",
+    },
+    {
+      id: 6,
+      title: "React-router",
+      url: "https://www.youtube.com/embed/fQoDUj46pwU?controls=0",
+      time: "1:30",
+      additionalText: "Additional text for video 1",
+      additionalTittle: "O6 Super Coins on the way",
+    },
+    {
+      id: 7,
+      title: "import and export",
+      url: "https://www.youtube.com/embed/fQoDUj46pwU?controls=0",
+      time: "1:30",
+      additionalText: "Additional text for video 1",
+      additionalTittle: "O6 Super Coins on the way",
+    },
+  ];
+
+  const handleVideoSelect = (video) => {
+    setSelectedVideo(video);
+    setAdditionalText(video.additionalText);
+    setAdditionalTittle(video.additionalTittle);
+  };
+
+  return (
+    <div>
+      <div className="flex">
+        <div className="w-1/4 bg-white">
+          <Link href="/" className="pl-[9rem]">
+            <Image
+              className=" pl-[2.5rem] "
+              src="/risetalkLogo.png"
+              alt="Example Image"
+              width={210}
+              height={140}
+            />
+          </Link>
+          <h1 className="text-gray-800 font-poppins text-2xl font-semibold  tracking-normal text-center  h-11 m-4">
+            Change Simplification
+          </h1>
+          <ul className="space-y-4 p-[2rem]">
+            {videos.map((video) => (
+              <li
+                key={video.id}
+                className="cursor-pointer text-gray-800 text-xs hover:bg-gray-800 hover:text-white relative py-[1rem] pl-[1rem] rounded-lg bg-orange-300 bg-opacity-30"
+                onClick={() => handleVideoSelect(video)}>
+                {video.title}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="w-3/4 bg-gray-200 relative ">
+          <div className="left-0 right-0 px-4 py-[1rem] bg-[#222129] text-white h-163 w-1423 left-497 top-0">
+            <h2 className="text-3xl font-poppins">
+              Learn about ReactJs from scratch
+            </h2>
+            <p className="font-poppins xs py-2">Introduction about ReactJs</p>
+            {selectedVideo && (
+              <div className="font-medium text-[0.875rem] flex-1 grid justify-items-end leading-[1.563rem] text-[#696984]">
+                {selectedVideo.time} Hour
+              </div>
+            )}
+          </div>
+          {selectedVideo ? (
+            <iframe
+              className="w-[79rem] h-[650px] py-[3rem] px-[3rem]"
+              src={selectedVideo.url}
+              width="100%"
+              height="650"
+              frameBorder="0"
+              allowFullScreen></iframe>
+          ) : (
+            <p className="text-[black] p-4">Plase Select a video</p>
+          )}
+          {selectedVideo && (
+            <div className="bg-[white] ">
+              <h1 className="font-poppins text-black text-2xl font-semibold leading-[5rem] tracking-normal px-[2rem] text-left">
+                {additionalTittle}
+              </h1>
+              <p className="font-poppins text-black text-xl font-normal leading-[27px] tracking-[0.02em] text-left py-2 px-[2rem]">
+                {additionalText}
+              </p>
+            </div>
+          )}
+          <section className="bg-[white] pt-[4rem] pb-[13.313rem]">
+            <div>
+              <ContainerCards title="More Courses" />
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CourseDetail;
--- app/mycourses/page.jsx
+++ app/mycourses/page.jsx
@@ -3,7 +3,6 @@ import Image from "next/image"
 import ContainerCards from "@/components/ContainerCards"
 import Navigation from "../../components/NavBar";
 
-
 export default function MyCoursesPage() {
 
     const nextLesson = [
@@ -22,7 +21,7 @@ export default function MyCoursesPage() {
             imgLesson: "https://s3-alpha-sig.figma.com/img/5abc/f770/24978c219bf2223fc9e32ec5fae8375f?Expires=1684713600&Signature=nDfFfKLVE0FyAjAZs~Fd8p~0-2UscFLAkJHKAzgqqmlO-2pmufwLh5EZtKT6qbcVS1K14FZZUBVFSTeboZPO6Q1puNQgyC7kMfBBJpGlAevkdWmMdvahShe5Etw5pATRD2HyBt-3H-purpQB895OlNzsHYGuXJzIZJc0MeJHp1JluxYciH7h1f52QnFcVg60RrV6pVnGde0FtbNDd247eH7iyB9gaACyvTIB1tJzLmtC~Mh8zHSENOQDWEy81hQzWZEbO40kK2Tjtjm0Z~Gywb2N1xYs~04LxiRBATylzbBxhJ6ZY7bTBBl7cg1PlUuZhoMfWhZdB6O4TK-80VAXeg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
         },
         {
-            id: "2",
+            id: "3",
             title: "AWS Certified Solutions Architect",
             author: "lina",
             imgAuthor: "https://s3-alpha-sig.figma.com/img/2f32/d3a9/082c2e2832481561feec93a5e5c5e8d6?Expires=1684713600&Signature=fK1-OcQsSO2cR29Qpk0bQcd64E5p32t0HeF9FXpAp-FyMJjJSPWaB4GME-ie7fAy07wbOA5aKYLpB1rgRnzjpHwAp51I2pF2NHe1~N1jbW0Dcm78CqrJSNjptwF1rno1UJ3U479IQ91C7awFUyZSSaVTWJhAXoqlYn5ZixtWn6vw6GQ-JH2stipNoAw7mBb21ZzMHFFSxQfrCT2r1k9BawBuBMvfKZx-eH8RCPgDqav~MuJab58k-Nhea7jgTjtPmdgxev~rkkpprduV30bwfhkQdMDOFsOnEEzA88ExKRFwsmm~57IGezBjmdAdzwYpjpDEdWBmxpiFQK99b6rKYw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
@@ -140,7 +139,8 @@ export default function MyCoursesPage() {
                                     return (
                                         <article className="w-[32%] bg-white px-[1.313rem] pt-[1.188rem] text-[black] rounded-[1.25rem] h-fit" key={lesson.id}>
                                             <Image className="w-[100%] h-[210px] object-cover rounded-[1.25rem]" src={lesson.imgLesson} alt={lesson.title} width={400} height={400} />
-                                            <h3 className="font-medium text-[1.25rem] leading-[2.7rem] mt-[0.75rem] mb-[0.55rem]">{lesson.title}</h3>
+
+                                            <Link href={`/mycourses/id`}> <h3 className="font-medium text-[1.25rem] leading-[2.7rem] mt-[0.75rem] mb-[0.55rem]">{lesson.title}</h3></Link >
                                             <div className="flex items-center  mb-[1.563rem] ">
                                                 <Image className="inline  bg-slate-500 h-[35px] w-[35px] object-cover object-top rounded-full" src={lesson.imgAuthor} alt={lesson.author} width={500} height={100} />
                                                 <span className="font-medium text-[1.2rem] leading-[1.688rem] ml-[0.938rem]">{lesson.author}</span>
