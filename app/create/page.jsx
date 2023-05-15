697a9739e6f526264fe58c9316c869bf48bf6f4f
JesseMcCallums <jessemccallums2001@gmail.com>
2023-05-14 21:05:11 -0400

    Add changes

---
+++ app/create/page.jsx
@@ -0,0 +1,13 @@
+import BestTeachers from '@/components/BestTeachers';
+import React from 'react';
+
+export default function Form() {
+ 
+
+  return (
+   <div>
+    <BestTeachers />
+   </div>
+    
+  )
+}
