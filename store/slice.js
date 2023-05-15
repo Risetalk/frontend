5205ffff99bc2aba3a6967a487b60860920d510d
Nahuel Chamorro <nahu.chamo19@gmail.com>
2023-05-14 21:44:04 -0300

    feature: Validation/Login

--- store/slice.js
+++ store/slice.js
@@ -2,56 +2,62 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   courses: [],
-  filterCourses:[]
+  filterCourses: [],
+  access: false,
 };
 
 export const Slice = createSlice({
   name: "courses",
   initialState,
-  
+
   reducers: {
+    loginAccess: (state, action) => {
+      state.access = action.payload;
+    },
     allCourses: (state, action) => {
       state.courses = action.payload;
       state.filterCourses = state.courses
     },
-    filterCoursesCategory:(state,action)=>{
-      if(action.payload === "all"){
+    filterCoursesCategory: (state, action) => {
+      if (action.payload === "all") {
         state.filterCourses = state.courses
-      }else {
-        state.filterCourses = [...state.courses.filter(course => {  
-          
+      } else {
+        state.filterCourses = [...state.courses.filter(course => {
+
           return course.category === action.payload
         })]
       }
     },
-    filterCoursesRaiting:(state,action)=>{
-      state.filterCourses = [...state.filterCourses.sort((a,b)=>{
-        if (action.payload === "min"){
+    filterCoursesRaiting: (state, action) => {
+      state.filterCourses = [...state.filterCourses.sort((a, b) => {
+        if (action.payload === "min") {
           return a.raiting > b.raiting
-        } 
-        else if (action.payload === "max"){
+        }
+        else if (action.payload === "max") {
           return a.raiting < b.raiting
-      }})
-    ]
+        }
+      })
+      ]
     },
 
-    filterCoursesOrder:(state,action)=>{
-      state.filterCourses = [...state.filterCourses.sort((a,b)=>{
-        if (action.payload === "a-z"){
+    filterCoursesOrder: (state, action) => {
+      state.filterCourses = [...state.filterCourses.sort((a, b) => {
+        if (action.payload === "a-z") {
           return a.title > b.title
-        } 
-        else if (action.payload === "z-a"){
+        }
+        else if (action.payload === "z-a") {
           return a.title < b.title
-      }})
-    ]
+        }
+      })
+      ]
     },
 
-    filterCoursesLanguaje:(state,action)=>{
-      if(action.payload === "all"){
+    filterCoursesLanguaje: (state, action) => {
+      if (action.payload === "all") {
         state.filterCourses = state.courses
-      }else {
-        state.filterCourses = [...state.courses.filter(course => {  
-          
+      } else {
+        state.filterCourses = [...state.courses.filter(course => {
+
           return course.languaje === action.payload
         })]
       }
@@ -60,5 +66,5 @@ export const Slice = createSlice({
   },
 });
 
-export const { allCourses , filterCoursesCategory, filterCoursesRaiting, filterCoursesOrder,filterCoursesLanguaje} = Slice.actions;
+export const { allCourses, filterCoursesCategory, filterCoursesRaiting, filterCoursesOrder, filterCoursesLanguaje,loginAccess } = Slice.actions;
 export default Slice.reducer;
