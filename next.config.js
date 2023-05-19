33f7b258d1f398e7711d57149df3ddcb4328e19d
Nahuel Chamorro <nahu.chamo19@gmail.com>
2023-05-19 16:42:22 -0300

    fix:configNext

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains:['s3-alpha-sig.figma.com','source.unsplash.com']
+        domains: ['s3-alpha-sig.figma.com', 'source.unsplash.com']
     },
+    devIndicators: {
+        autoPrerender: false,
+    },
+    port: 3000,
 }
 
 module.exports = nextConfig
