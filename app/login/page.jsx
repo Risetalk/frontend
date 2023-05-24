9da665992181fa220b0164f4d71683219a8e104f
elizabeth ortiz <elizabeth-ortiz-rivas@hotmail.es>
2023-05-23 20:27:45 -0500

    Feature-LoginAuT
    in this commit.
    -Login page Route conexion with server succesful
    -Validation  of Email and password
    -Confirmate page view was connect with the server
    -instalation of Google Auth
    -Google Auth workng

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,134 +1,236 @@
-'use client'
+"use client";
 
+import react from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useSelector, useDispatch } from "react-redux";
 import { motion } from "framer-motion";
-
+import axios from "axios";
 import { useRouter } from "next/navigation";
-
-
 import { useEffect, useState } from "react";
-
 import validation from "./validation";
+import { GoogleLogin } from "react-google-login";
+import { gapi } from "gapi-script";
 import { loginAccess } from "@/store/slice";
 
+const Login = () => {
+  const [gapiLoaded, setGapiLoaded] = useState(false);
+  const clientId =
+    "234473629746-soigamg3hnt1n0gd6v192baikfqmgi2m.apps.googleusercontent.com";
 
-export default  function Login() {
-    const dispatch = useDispatch()
-    const courses = useSelector((state) => state.courses);
+  const dispatch = useDispatch();
+  const courses = useSelector((state) => state.courses);
+  const router = useRouter();
+  const [access, setAccess] = useState(false);
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
 
-    const router = useRouter(); 
-    const username = 'nahucham';
-    const password = '1234asdfg';
+  const [errors, setErrors] = useState({
+    email: "no",
+    password: "no",
+  });
 
+  const handleInputChange = (event) => {
+    setUserData({
+      ...userData,
+      [event.target.name]: event.target.value,
+    });
 
-    const [ access, setAccess] = useState (false)
-    const [ userData, setUserData] = useState({
-        username:'',
-        password:''
-    })
+    setErrors(
+      validation({
+        ...userData,
+        [event.target.name]: event.target.value,
+      })
+    );
+  };
 
-    const [ errors, setErrors] = useState({
-        username:'no',
-        password:'no'
-    })
-
-    const handleInputChange = (event) =>{
-        setUserData({
-            ...userData,
-            [event.target.name]: event.target.value
-        })
-
-        setErrors(validation({
-            ...userData,
-            [event.target.name] : event.target.value
-        }))
+  const handleOnSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:3001/user/login",
+        userData
+      );
+      console.log(response);
+    } catch (error) {
+      console.error("Error getting data:", error);
     }
 
-    const handleOnSubmit = (event) =>{
-
-        const hasErrors = Object.values(errors).every(error => error === "no")
-        if (hasErrors && (userData.username === username && userData.password === password)){
-            
-            dispatch(loginAccess(true))
-            event.preventDefault()
-        }else{
-            
-                window.alert('Error al ingresar datos')
-                
-            
-        }
-
+    const hasErrors = Object.values(errors).every((error) => error === "no");
+    if (
+      hasErrors &&
+      userData.email === userData.email &&
+      userData.password === userData.password
+    ) {
+      dispatch(loginAccess(true));
+      event.preventDefault();
+    } else {
+      window.alert("Error al ingresar datos");
     }
+  };
 
+  useEffect(() => {
+    // if(courses.access){
+    //     router.push('/')
+    // }
+  }, [courses.access]);
 
-    useEffect(()=>{
-        
-        // if(courses.access){
-        //     router.push('/')
-        // }
-    },[courses.access])
+  useEffect(() => {
+    const start = () => {
+      gapi.load("client:auth2", () => {
+        gapi.auth2
+          .init({
+            clientId,
+          })
+          .then(() => {
+            setGapiLoaded(true);
+          });
+      });
+    };
+    if (typeof window !== "undefined") {
+      start();
+    }
+  }, []);
 
-    return (
-        <div className="flex text-black  py-[0.5rem] h-[100vh]">
-            <motion.div className="w-[50%]"
-            initial={{opacity:0, x:-50}}
-            animate={{opacity:1 ,x:0}}
-            transition={{duration:1}}>
-                
-                <Image src={"https://s3-alpha-sig.figma.com/img/8254/7cef/95aac6fc03724e9cf48466ee2c65e717?Expires=1685923200&Signature=AVdO~PF8E4vRiVNO-v~ljA1JH9P9dDT4TLpzO6aWRVeGvT-55Yo93duLBf0HNKp7XYRt2YbY1DQWhoV-QGD8~cb6UyCY9ZFWOj1SM6QMZLdimg~84Bt6RXYN7i2LFVTKxIcU0C80v727iAqfyZiaRrdu-usXg-kxnPccHX7zBlR8Fzc-lM5sSkI4WqjatYXka8U3m~pcCem4mubxv-KrXhfILkSSQ44CE2xHTknouD2YXYY0gD3ISflbvSfEx841L8ovNbF2Op23aGuRd-LBx-7dlzYDrQZk4MR3m9RnMKFckR0~CRNxwxlxTwzhbPfTJHOysRUgs62ST8WSRXR~Sw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"}
-                    alt="img-login" width={800} height={500}
-                    className="h-[100%] w-[80%] object-cover object-right rounded-r-3xl"
-                />
-            </motion.div>
-            <form action="" className=" w-[50%] h-[100%]" onSubmit={handleOnSubmit}>
-                <div  className="flex flex-col items-center justify-between w-[50%] mx-[auto]">
-                    <Image onClick={()=>{router.push('/')}} src={"https://s3-alpha-sig.figma.com/img/4912/c528/b482053e4d5d1d82d7eed9e4ea91dd17?Expires=1685923200&Signature=bQ4vf5Y082FcVS1B7vbU2tdMUYDANSUu4XxsohDBFvSI94wLoe~uvY0mGd9Fd~Vck77GsGz76hPL0uXEj9XntjAb~zXNC2xwSf0nEnEDig8-xzkOyH~DpfxX9UHluna1Wq79mcq7czivvaF0Hf3B8hZAO3I9kwRDibzIX4paCkJ7pNCnFDbL0eg6dD-5zARCtbdmgnHiZDtGW1bgSRLE2AJX631U65eh9NmRCf8-8OuZexg9IlSD92W0xR8mkMnQT4ZbdlS8pnLyJK9ZfdXzvbesiqG-sK7cUZ5uWBmZ4gDsmAqGZImHIMlPORqzjOWn52hnVh1cEXu0XFkpWUMCGg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"}
-                        alt="img-logo"
-                        width={300}
-                        height={300}
-                        className="cursor-pointer"/>
-                    <span className="font-normal text-[1rem] leading-[1.5rem]">Welcome to RiseTalk</span>
-                    <div className="flex py-[0.625rem] px-[0.75rem] gap-x-4 rounded-[2rem] bg-[#222129A6] my-[3.25rem]">
-                        <Link href={`/login`} className="font-medium text-4 leading-[2.5rem] text-white px-[3rem] rounded-[2rem] bg-[#222129] ">Login</Link>
-                        <Link href={`/register`} className="font-medium text-4 leading-[2.5rem] text-white px-[3rem] rounded-[2rem] ">Register</Link>
-                    </div>
+  const responseSuccessGoogle = async (response) => {
+    // console.log(response)
+    await axios
+      .post("http://localhost:3001/user/googlelogin", {
+        tokenId: response.tokenObj.id_token,
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
+  const responseErrorGoogle = (response) => {};
 
-                    {/* <p className="font-normal text-[1rem] text-start leading-[1.5rem] text-[#5B5B5B] mb-[2.5rem]">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p> */}
-                    <div className="flex flex-col w-[100%]">
+  return (
+    <div>
+      <div className="flex text-black  py-[0.5rem] h-[100vh]">
+        <motion.div
+          className="w-[50%]"
+          initial={{ opacity: 0, x: -50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}>
+          <Image
+            src={
+              "https://s3-alpha-sig.figma.com/img/8254/7cef/95aac6fc03724e9cf48466ee2c65e717?Expires=1685923200&Signature=AVdO~PF8E4vRiVNO-v~ljA1JH9P9dDT4TLpzO6aWRVeGvT-55Yo93duLBf0HNKp7XYRt2YbY1DQWhoV-QGD8~cb6UyCY9ZFWOj1SM6QMZLdimg~84Bt6RXYN7i2LFVTKxIcU0C80v727iAqfyZiaRrdu-usXg-kxnPccHX7zBlR8Fzc-lM5sSkI4WqjatYXka8U3m~pcCem4mubxv-KrXhfILkSSQ44CE2xHTknouD2YXYY0gD3ISflbvSfEx841L8ovNbF2Op23aGuRd-LBx-7dlzYDrQZk4MR3m9RnMKFckR0~CRNxwxlxTwzhbPfTJHOysRUgs62ST8WSRXR~Sw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+            }
+            alt="img-login"
+            width={800}
+            height={500}
+            className="h-[100%] w-[80%] object-cover object-right rounded-r-3xl"
+          />
+        </motion.div>
+        <form action="" className=" w-[50%] h-[100%]" onSubmit={handleOnSubmit}>
+          <div className="flex flex-col items-center justify-between w-[50%] mx-[auto]">
+            <Image
+              onClick={() => {
+                router.push("/");
+              }}
+              src={
+                "https://s3-alpha-sig.figma.com/img/4912/c528/b482053e4d5d1d82d7eed9e4ea91dd17?Expires=1685923200&Signature=bQ4vf5Y082FcVS1B7vbU2tdMUYDANSUu4XxsohDBFvSI94wLoe~uvY0mGd9Fd~Vck77GsGz76hPL0uXEj9XntjAb~zXNC2xwSf0nEnEDig8-xzkOyH~DpfxX9UHluna1Wq79mcq7czivvaF0Hf3B8hZAO3I9kwRDibzIX4paCkJ7pNCnFDbL0eg6dD-5zARCtbdmgnHiZDtGW1bgSRLE2AJX631U65eh9NmRCf8-8OuZexg9IlSD92W0xR8mkMnQT4ZbdlS8pnLyJK9ZfdXzvbesiqG-sK7cUZ5uWBmZ4gDsmAqGZImHIMlPORqzjOWn52hnVh1cEXu0XFkpWUMCGg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+              }
+              alt="img-logo"
+              width={300}
+              height={300}
+              className="cursor-pointer"
+            />
+            <span className="font-normal text-[1rem] leading-[1.5rem]">
+              Welcome to RiseTalk
+            </span>
+            <div className="flex py-[0.625rem] px-[0.75rem] gap-x-4 rounded-[2rem] bg-[#222129A6] my-[2.80rem]">
+              <Link
+                href={`/login`}
+                className="font-medium text-4 leading-[2.5rem] text-white px-[3rem] rounded-[2rem] bg-[#222129] ">
+                Login
+              </Link>
+              <Link
+                href={`/register`}
+                className="font-medium text-4 leading-[2.5rem] text-white px-[3rem] rounded-[2rem] ">
+                Register
+              </Link>
+            </div>
 
-                        <label htmlFor="">
-                            Username
-                            <div>
-                                <input name="username" type="text" placeholder="Enter you Username" className="font-light text-[0.9rem] leading-[1.3rem] py-[1rem] px-[2rem] rounded-[2.5rem] w-[100%] mt-[0.75rem] mb-[0.2rem]   border-2 border-[#222129]" onChange={handleInputChange} />
-                            <h6 className={`${errors.username === 'no' ? 'text-white' :  'text-red-400'} mb-[2rem]`}>{errors.username}</h6>
-                            </div>
-                        </label>
-
-                        <label htmlFor="">
-                            Password
-                            <div>
-                                <input name="password" type="password" placeholder="Enter you Password" className="font-light text-[0.9rem] leading-[1.3rem] py-[1rem] px-[2rem] rounded-[2.5rem] w-[100%] mt-[0.75rem] mb-[0.2rem] border-2 border-[#222129]" onChange={handleInputChange}/>
-                                <h6 className={`${errors.password === 'no' ? 'text-white' :  'text-red-400'} mb-[2rem]`}>{errors.password}</h6>
-                            </div>
-                        </label>
-
-                    </div>
-
-                    <div className="flex justify-between w-[100%] font-light text-[0.75rem] leading-[1.125rem] px-[0.4rem]">
-                        <div >
-                            <input type="checkbox" name="" id="" className="mr-[0.5rem]"/>
-                            Remember me
-                        </div>
-                        <Link href={"/login/forgetpassword"}>Forgot Password?</Link>
-                    </div>
-
-                    <div className="flex w-[100%] justify-end ">
-                        <button onClick={()=>{router.push('/mycourses')}} className="font-normal text-[1.125rem] leading-[1.5rem] text-white py-[0.8rem]  px-[4.5rem] bg-[#222129] rounded-[2.25rem] mt-[3.75rem]">Login</button>
-                    </div>
+            {/* <p className="font-normal text-[1rem] text-start leading-[1.5rem] text-[#5B5B5B] mb-[2.5rem]">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p> */}
+            <div className="flex flex-col w-[100%]">
+              <label htmlFor="">
+                Email
+                <div>
+                  <input
+                    name="email"
+                    value={userData.email}
+                    type="text"
+                    placeholder="Enter your email"
+                    className="font-light text-[0.9rem] leading-[1.3rem] py-[1rem] px-[2rem] rounded-[2.5rem] w-[100%] mt-[0.75rem] mb-[0.2rem]   border-2 border-[#222129]"
+                    onChange={handleInputChange}
+                  />
+                  <h6
+                    className={`${
+                      errors.email === "no" ? "text-white" : "text-red-400"
+                    } mb-[2rem]`}>
+                    {errors.email}
+                  </h6>
                 </div>
-            </form>
-        </div>
-    )
-}
+              </label>
+
+              <label htmlFor="">
+                Password
+                <div>
+                  <input
+                    name="password"
+                    value={userData.password}
+                    type="password"
+                    placeholder="Enter you Password"
+                    className="font-light text-[0.9rem] leading-[1.3rem] py-[1rem] px-[2rem] rounded-[2.5rem] w-[100%] mt-[0.75rem] mb-[0.2rem] border-2 border-[#222129]"
+                    onChange={handleInputChange}
+                  />
+                  <h6
+                    className={`${
+                      errors.password === "no" ? "text-white" : "text-red-400"
+                    } mb-[2rem]`}>
+                    {errors.password}
+                  </h6>
+                </div>
+              </label>
+            </div>
+
+            <div className="flex justify-between w-[100%] font-light text-[0.75rem] leading-[1.125rem] px-[0.4rem]">
+              <div>
+                <input type="checkbox" name="" id="" className="mr-[0.5rem]" />
+                Remember me
+              </div>
+              <Link href={"/login/forgetpassword"}>Forgot Password?</Link>
+            </div>
+
+            <div className="flex w-[100%] justify-end mb-4">
+              <button
+                onClick={() => {
+                  router.push("/mycourses");
+                }}
+                className="font-normal text-[1.125rem] leading-[1.5rem] text-white py-[0.8rem]  px-[4.5rem] bg-[#222129] rounded-[2.25rem] mt-[1.75rem]">
+                Login
+              </button>
+            </div>
+            <hr className=" w-[30rem] bg-gray-200" />
+
+            <div className=" leading-[2rem] px-[3rem] rounded-[4rem] bg-[#222129A6] m-[1rem] ">
+              <div className=" py-[1rem]">
+                <GoogleLogin
+                  clientId={clientId}
+                  buttonText="Login with Google"
+                  onSuccess={responseSuccessGoogle}
+                  onFailure={responseErrorGoogle}
+                  cookiePolicy={"single_host_origin"}
+                />
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default Login;
