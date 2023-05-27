1f6c3da04059762a2768d9af0e63a58930df333a
Nahuel Chamorro <nahu.chamo19@gmail.com>
2023-05-26 21:16:47 -0300

    fix:DetailCourseById/AddStateGlobal/Summary

--- components/Summary.jsx
+++ components/Summary.jsx
@@ -7,9 +7,7 @@ import Image from "next/image"
 export default function Summary() {
 
     const infoToBuyCourses = useSelector((state) => state.courses);
-    const dataMyCart = infoToBuyCourses.my_cart[0];
-
-    console.log(dataMyCart);
+    const dataMyCart = infoToBuyCourses.my_cart;
 
     const productos = [
         {
@@ -29,59 +27,62 @@ export default function Summary() {
     ]
 
     return (
-        <article className="w-[35%] bg-[#002B5A20] rounded-[1.25rem] m-auto mt-4 p-[1.8rem]">
-            <h3 className="mb-[2rem]">Summary</h3>
-            <div className="productos">
-                {/* {
-                    productos.map(product => (
-                        <div className="flex gap-x-[1.25rem] w-[ 100%] border-b-[0.5px] border-b-stone-400" key={product.id}>
-                            <div className="w-[30%] py-[0.6rem] ">
-                                <Image className="rounded-[1.25rem]" src={product.imagaCourse} alt="img-product" width={400} height={200} />
-                            </div>
-                            <div className="">
-                                <h3 className="font-normal text-[0.8rem] leading-[2rem] text-[#000000]">{product.title}</h3>
-                                <p className="font-normal text-[0.8rem] leading-[2rem] text-[#5B5B5B]">{product.description}</p>
-                                <h4 className="font-normal text-[1.2rem] leading-[2.6rem] text-[#000000]">{product.price}</h4>
-                            </div>
 
+        <article className="h-[100vh] pt-[4rem]">
+            <div className="w-[35%] bg-[#002B5A20] rounded-[1.25rem] m-auto mt-4 p-[1.8rem]">
+                <h3 className="mb-[2rem]">Summary</h3>
+                <div className="productos">
+                    {/* {
+                        productos.map(product => (
+                            <div className="flex gap-x-[1.25rem] w-[ 100%] border-b-[0.5px] border-b-stone-400" key={product.id}>
+                                <div className="w-[30%] py-[0.6rem] ">
+                                    <Image className="rounded-[1.25rem]" src={product.imagaCourse} alt="img-product" width={400} height={200} />
+                                </div>
+                                <div className="">
+                                    <h3 className="font-normal text-[0.8rem] leading-[2rem] text-[#000000]">{product.title}</h3>
+                                    <p className="font-normal text-[0.8rem] leading-[2rem] text-[#5B5B5B]">{product.description}</p>
+                                    <h4 className="font-normal text-[1.2rem] leading-[2.6rem] text-[#000000]">{product.price}</h4>
+                                </div>
+            
+                            </div>
+                        ))
+                    } */}
+
+                    <div className="flex gap-x-[1.25rem] w-[ 100%] border-b-[0.5px] border-b-stone-400" key={dataMyCart.id}>
+                        <div className="w-[30%] py-[0.6rem] ">
+                            <Image className="rounded-[1.25rem]" src={dataMyCart.background_image} alt="img-product" width={400} height={200} />
                         </div>
-                    ))
-                } */}
-                  
-                        <div className="flex gap-x-[1.25rem] w-[ 100%] border-b-[0.5px] border-b-stone-400" key={dataMyCart.id}>
-                            <div className="w-[30%] py-[0.6rem] ">
-                                <img className="rounded-[1.25rem]" src={dataMyCart.background_image} alt="img-product" width={400} height={200} />
-                            </div>
-                            <div className="">
-                                <h3 className="font-normal text-[0.8rem] leading-[2rem] text-[#000000]">{dataMyCart.title}</h3>
-                                <p className="font-normal text-[0.8rem] leading-[2rem] text-[#5B5B5B]">{dataMyCart.description}</p>
-                                <h4 className="font-normal text-[1.2rem] leading-[2.6rem] text-[#000000]">{dataMyCart.price} $</h4>
-                            </div>
-
+                        <div className="">
+                            <h3 className="font-normal text-[0.8rem] leading-[2rem] text-[#000000]">{dataMyCart.title}</h3>
+                            <p className="font-normal text-[0.8rem] leading-[2rem] text-[#5B5B5B]">{dataMyCart.description}</p>
+                            <h4 className="font-normal text-[1.2rem] leading-[2.6rem] text-[#000000]">{dataMyCart.price} $</h4>
                         </div>
-                    
 
-                <ul>
-                    <li className="flex justify-between border-b-[0.5px] border-b-stone-400  py-[0.6rem] font-semibold text-[1rem] leading-[2rem] text-[#5B5B5B]">
-                        <span>Subtotal</span>
-                        <span>{dataMyCart.price} $</span>
-                    </li>
-                    {/* <li className="flex justify-between border-b-[0.5px] border-b-stone-400  py-[0.6rem] font-semibold text-[1rem] leading-[2rem] text-[#5B5B5B]">
-                        <span>Coupon Discount</span>
-                        <span>0 %</span>
-                    </li>
-                    <li className="flex justify-between border-b-[0.5px] border-b-stone-400  py-[0.6rem] font-semibold text-[1rem] leading-[2rem] text-[#5B5B5B]">
-                        <span>TAX</span>
-                        <span>5</span>
-                    </li> */}
-                    <li className="flex justify-between py-[0.6rem] font-semibold text-[1rem] leading-[2rem] text-[#000000]">
-                        <span>Total</span>
-                        <span>{dataMyCart.price} $</span>
-                    </li>
-                    <li>
-                        <CheckoutPage course={dataMyCart} />
-                    </li>
-                </ul>
+                    </div>
+
+
+                    <ul>
+                        <li className="flex justify-between border-b-[0.5px] border-b-stone-400  py-[0.6rem] font-semibold text-[1rem] leading-[2rem] text-[#5B5B5B]">
+                            <span>Subtotal</span>
+                            <span>{dataMyCart.price} $</span>
+                        </li>
+                        {/* <li className="flex justify-between border-b-[0.5px] border-b-stone-400  py-[0.6rem] font-semibold text-[1rem] leading-[2rem] text-[#5B5B5B]">
+                            <span>Coupon Discount</span>
+                            <span>0 %</span>
+                        </li>
+                        <li className="flex justify-between border-b-[0.5px] border-b-stone-400  py-[0.6rem] font-semibold text-[1rem] leading-[2rem] text-[#5B5B5B]">
+                            <span>TAX</span>
+                            <span>5</span>
+                        </li> */}
+                        <li className="flex justify-between py-[0.6rem] font-semibold text-[1rem] leading-[2rem] text-[#000000]">
+                            <span>Total</span>
+                            <span>{dataMyCart.price} $</span>
+                        </li>
+                        <li>
+                            <CheckoutPage course={dataMyCart} />
+                        </li>
+                    </ul>
+                </div>
             </div>
         </article>
     )
