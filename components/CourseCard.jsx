89a116d206ccabee36be44fe678ab1ae3693a92b
Nahuel Chamorro <nahu.chamo19@gmail.com>
2023-05-27 20:51:22 -0300

    fix:NavBar/Footer/DarkMode/Tippy/CourseBuyDetail

--- components/CourseCard.jsx
+++ components/CourseCard.jsx
@@ -1,101 +1,130 @@
+'use client'
+
 import Image from "next/image"
 import Link from "next/link"
 import { motion } from "framer-motion"
+import likeIco from '../public/like.png'
 
-import { useRef, useEffect } from "react";
-import tippy from 'tippy.js';
+import { useRef, useEffect, useState } from "react";
+import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
-import 'tippy.js/themes/light.css';
-
-import { createPortal } from 'react-dom';
+import axios from "axios";
 
 
+// id = { course.id }
+// key = { index }
+// title = { course?.title }
+// background_image = { course?.background_image }
+// description = { course?.description }
+// categoryId = { course?.categoryId }
+// laguage = { course?.language }
+// createdAt = { course?.createdAt }
+// updatedAt = { course?.updatedAt }
+// price = { course?.price }
+// like = { course?.like }
 
-export default function CourseCard({ id, title, background_image, tema, duration, description, author, price, offer }) {
 
-    const cardRef = useRef(null);
-    const tooltipContentRef = useRef(null);
+export default function CourseCard({ id, title, background_image, categoryId, language, description, createdAt, price, updatedAt, like }) {
+    ;
 
+    // const ref = useRef(null)
+
+    // const tooltipContent = (
+    //     <div className="text-white" ref={tooltipContentRef}>
+    //         <h2 className="font-bold text-[1.6rem]">{title}</h2>
+    //         <h3 className="text-[#F9662A]">$ {price}</h3>
+    //         <p className="font-normal ">{description}</p>
+    //     </div>
+    // );
     useEffect(() => {
-        if (cardRef.current && tooltipContentRef.current) {
-            tippy(cardRef.current, {
-                placement: "left",
-                content: tooltipContentRef.current,
-                allowHTML: true,
-                arrow: true
-            });
-        }
-    }, []);
-
-    const tooltipContent = (
-        <div className="text-white" ref={tooltipContentRef}>
-            <h2 className="font-bold text-[1.6rem]">{title}</h2>
-            <h3 className="text-[#F9662A]">$ {price}</h3>
-            <p className="font-normal ">{description}</p>
-        </div>
-    );
-
+    }, [])
 
     return (
         <div className="card" >
-            {createPortal(tooltipContent, document.body)}
-            <motion.article
-                ref={cardRef}
-                initial={{ opacity: 0, y: 200 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1, delay: 0.5 }}
-                className="flex flex-col justify-between pt-4 px-6 bg-[white] pb-4  rounded-[1.25rem]  shadow-2xl h-[500px]"
-                key={id}>
-                <Image
-                    className="rounded-[1.25rem] h-[200px] object-cover w-[100%]"
-                    src={background_image}
-                    alt={title || "img-course"}
-                    width={300}
-                    height={300}
-                />
-                <div className="flex justify-between my-[1rem]">
-                    <span className="font-medium text-[0.875rem] leading-[1.563rem] text-[#d77631]">
-                        {tema}
-                    </span>
-                    <span className="font-medium text-[0.875rem] leading-[1.563rem] text-[#696984]">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            className="inline-block w-4 h-4 mr-1">
-                            <circle cx="12" cy="12" r="10" />
-                            <path d="M12 6v6l4 2" />
-                        </svg>
-                        {duration} Month
-                    </span>
-                </div>
-                <Link href={`search/${id}`} className="font-medium text-[1.3rem] leading-[1.9rem] text-[#252641] mb-[1.2rem]">
-                    {title}
-                </Link>
-                <p className="font-normal text-[1rem] leading-[1.6rem] text-[#696984] mb-[1.063rem] h-[80px] overflow-y-scroll">
-                    {description}
-                </p>
-                <div className="flex items-end justify-between">
-                    <div className="flex items-center">
-                        <Image className="rounded-full" src={"https://source.unsplash.com/64x64/?person"} alt="logo" width={36} height={36} />
-                        <span className="font-medium text-[1rem] leading-[1rem] ml-[0.938rem] text-[black]">
-                            {author}
-                        </span>
+            <Tippy
+                placement="right"
+                content={
+                    <div className="flex flex-col p-[1rem] gap-y-[1rem]">
+                        <h3 className="font-bold text-[1.6rem]">{title}</h3>
+                        <span className="text-[0.8rem]">Update last: { updatedAt? updatedAt.slice(0,10) : ""}</span>
+                        <p className="font-medium text-[1rem]">{description}</p>
+                        <button className="font-bold text-[1.2rem] bg-[#F9662A] py-[0.6rem] w-[90%] mx-[auto] rounded-[0.6rem]">Add to carr</button>
                     </div>
-                    <div>
-                        <span className="font-light italic line-through text-[1rem] leading-[1.688rem] mr-4 text-[#00000080]">
-                            ${price}
-                        </span>
-                        <span className="font-bold text-[1rem] leading-[2.25rem] text-[#222129] ">
-                            ${offer}
-                        </span>
+                }
+
+
+            >
+                {/* {tooltipContent && createPortal(tooltipContent, document.body)} */}
+                <motion.article
+                    // ref={cardRef}
+                    initial={{ opacity: 0, y: 200 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 1, delay: 0.5 }}
+                    className="flex flex-col justify-between  bg-[white] h-[300px]   shadow-2xl "
+                    key={id}>
+                    <Image
+                        className=" h-[100%] object-cover w-[100%]"
+                        src={background_image}
+                        alt={title || "img-course"}
+                        width={300}
+                        height={400}
+                    />
+                    <div className="h-[50%] flex flex-col p-[0.6rem] justify-between">
+                        <Link href={`search/${id}`} className="font-medium text-[1.3rem] leading-[1.9rem] text-[#252641] ">
+                            {title}
+                        </Link>
+                        <span className="text-[0.8rem]">Create date:{ createdAt? createdAt.slice(0,10) : ""}</span>
+                        <div className="flex justify-between">
+                            <span className="font-medium text-[0.8rem] leading-[1.563rem] text-[#d77631]">
+                                Category
+                            </span>
+                            <span className="text-[0.8rem]">{language}</span>
+                        </div>
+                        <div className="flex items-center gap-x-[0.4rem]">
+                            <Image
+                                src={likeIco} alt="like-ico" width={60} height={60}
+                                className="w-[20px] h-[20px]" />
+                            <span className="font-bold text-[#909090] text-[1rem]">{like}</span>
+                        </div>
+
+
+
+                        {/* <span className="font-medium text-[0.875rem] leading-[1.563rem] text-[#696984]">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    className="inline-block w-4 h-4 mr-1">
+                                    <circle cx="12" cy="12" r="10" />
+                                    <path d="M12 6v6l4 2" />
+                                </svg>
+                                {duration} Month
+                        
+                            </span> */}
+
+                        {/* <p className="font-normal text-[1rem] leading-[1.6rem] text-[#696984] mb-[1.063rem] h-[80px] overflow-y-scroll">
+                            {description}
+                        </p> */}
+                        <div className="flex items-end justify-between">
+                            {/* <div className="flex items-center">
+                                <Image className="rounded-full" src={"https://source.unsplash.com/64x64/?person"} alt="logo" width={36} height={36} />
+                                <span className="font-medium text-[1rem] leading-[1rem] ml-[0.938rem] text-[black]">
+                                    {author}
+                                </span>
+                            </div> */}
+                            <div>
+                                <span className="font-bold text-[1.2rem] leading-[2.25rem] text-[#222129] ">
+                                    $ {price}
+                                </span>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </motion.article>
+                </motion.article>
+            </Tippy>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        
+        // Light Mode
+        "burnt-orange": "#F9662A", 
+        "midnight-shadow":"#222129",
+        "blue-gray":"#B2B3CF",
+        "dusty-gray":"#7F7A8D",
+        "cloud-gray":"#E0E0E0",
+
+        // Dark Mode
+        "midnight-blue":"#234B75",
+      },
+      height:{
+
+        '40vh':"40vh"
+
+      },
+
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
+
