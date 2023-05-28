6c91143ab37dc4183abaf2ad179978f123effdcd
elizabeth ortiz <elizabeth-ortiz-rivas@hotmail.es>
2023-05-27 19:10:51 -0500

    Feature/LocalStorage
    in this commit
    -LocalStorage implementacion with users
    -Validations and alerts to forget password component
    - JWS to login users
    -instalation of cookies and Jose for Auth users

--- app/register/confirmated/[id]/page.jsx
+++ app/register/confirmated/[id]/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import Register from "@/components/Register";
+import Register from "@/components/Registrer/Register";
 import check from "../../../../public/check.png";
 import Image from "next/image";
 import Link from "next/link";
