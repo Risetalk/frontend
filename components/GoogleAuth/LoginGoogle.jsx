93e50b1b344c599f3438b3a1f12ef939fb8cace4
Diego Ibañez <diegoibanez336@gmail.com>
2023-05-29 18:03:51 -0500

    front/UserAuth (front/UserAuth): permissions are configured to register with google data is stored in localStorage

--- components/GoogleAuth/LoginGoogle.jsx
+++ components/GoogleAuth/LoginGoogle.jsx
@@ -4,10 +4,22 @@ import { useSession, signIn, signOut } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { usePathname } from "next/navigation";
-
+import axios from "axios";
 const LoginGoogle = () => {
   const { data: session } = useSession();
   const router = useRouter();
+
+  console.log(session)
+
+  const handlerGoogle = async () => { 
+    signIn(null, { callbackUrl: "/" })
+    try {      
+      await axios.post("/api/auth/loginGoogle", session);
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  }
+
 
   if (session) {
     return <div></div>;
@@ -23,7 +35,7 @@ const LoginGoogle = () => {
               alt="google logo"
               className="mr-4"
             />
-            <button onClick={() => signIn(null, { callbackUrl: "/" })}>
+            <button onClick={handlerGoogle}>
               Sign In with Google
             </button>
           </div>
