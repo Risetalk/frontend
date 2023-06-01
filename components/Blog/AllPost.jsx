9d955daed7b6a7e11ff1d2bde2d500dff4e576e0
Diegoc24 <carusodiegod@gmail.com>
2023-06-01 07:00:38 -0300

    feature/createBlog(Frontend/createBlog) blog created

--- components/Blog/AllPost.jsx
+++ components/Blog/AllPost.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
 import { setPostUser } from "@/store/slice";
@@ -10,7 +10,7 @@ import { motion } from "framer-motion";
 // import AddCommentPost from "./addCommentPost";
 import { useSession } from "next-auth/react";
 import { store } from "@/store/store";
-
+import { useEffect } from "react";
 export default function AllPost() {
   const dispatch = useDispatch();
   const [formComment, setFormComment] = useState({
@@ -19,12 +19,20 @@ export default function AllPost() {
     postId: ""
   })
   const { data: session, status } = useSession();
-  
+  const [storeUser, setStoreUser] = useState(null);
   const post = useSelector((state) => state.courses);
-  const storeUser = localStorage.getItem("user");
-  console.log(storeUser);
- 
+
+
+useEffect(() => {
+  if (typeof window !== "undefined") {
+   
+    const userData = localStorage.getItem("userGoogle");
+    setStoreUser(userData ? JSON.parse(userData) : null);
+  }
+}, []);
+
     
+  
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -36,31 +44,39 @@ export default function AllPost() {
     };
 
     fetchData();
-  }, [dispatch]);
-     
-    
-  const handlerSubmit = async (id) => {
+  }, [dispatch]);  
+  
+  const handlerSubmit = (id) => {
+   
     setFormComment({
       ...formComment,
+      userId: storeUser?.id,
       postId: id
     });
   };
-
+  
+  const postData = async () => {
+    console.log("entra");
+    try {
+      const dataToSend = {
+        ...formComment,
+        userId: storeUser?.id
+      };
+      console.log(dataToSend);
+      await axios.post("http://localhost:3001/postcomment", dataToSend);
+    } catch (error) {
+      console.log({ message: error.message });
+    }
+  };
+  
   useEffect(() => {
-    const postData = async () => {
-      try {
-       
-        await axios.post("http://localhost:3001/postcomment", formComment);
-      } catch (error) {
-        console.log({ message: error.message });
-      }
-    };
-
-    if (formComment.postId !== "") {
+    if (formComment.postId !== "" && (formComment.userId !== null || formComment.userId !== undefined)) {
       postData();
     }
   }, [formComment]);
-
+  
+ 
+  
 const handlerChange = (e)=>{
   e.preventDefault();
   const {name, value} = e.target
@@ -77,40 +93,31 @@ const [isOpen, setIsOpen] = useState(false);
     setIsOpen(!isOpen);
   };
 
-// const handleEdit = () => {
-//   // Lógica para manejar la opción "Editar"
-//   console.log("Editar opción seleccionada");
-// };
-
 const handleDelete = (id) => {
-  // Lógica para manejar la opción "Eliminar"
-  const deleteData = () =>{
-    console.log(id);
-  }
+
+ 
+    const deleteData = async () =>{
+      try {
+        console.log(id);
+        const response = await axios.delete(`http://localhost:3001/post/${id}`)
+        console.log(response);
+      } catch (error) {
+        console.log({message: error.message});
+      }
+      
+    }
+ 
+  
   deleteData()
 };
     const getPost = post.postUser
 
 
     return (
-      <div className="flex flex-col justify-center items-center mt-8">
+      <div className="flex flex-col justify-center items-center mt-8 border-b border-gray-500">
         {getPost?.map((postItem) => (
-          <div key={postItem.first_name} style={{ width: '75vw' }} className="bg-gray-200 p-10 m-2">
-            <div className="flex items-start">
-              <Image
-                src={postItem.profile_picture}
-                alt={postItem.profile_picture}
-                className="inline ... rounded-full"
-                width={45}
-                height={45}
-              />
-              <div className="inline text-left ml-2">
-                <div>{postItem.first_name}</div>
-                <div>{postItem.last_name}</div>
-                
-              </div>
-              
-            </div>
+          <div key={postItem.first_name} style={{ width: '75vw' }} className=" p-10 m-2">
+           
             <div>
               {
                 postItem.email === session?.user.email ?
@@ -127,7 +134,7 @@ const handleDelete = (id) => {
       {isOpen && (
         <motion.ul layout>
          
-          <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} onClick={()=>handleDelete(postItem.id)}>
+          <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} onClick={()=>handleDelete(postItem.post.id)}>
             Eliminar
           </motion.li>
         </motion.ul>
@@ -136,8 +143,8 @@ const handleDelete = (id) => {
               }
             
     </div>
-            <div>{postItem.post.title}</div>
-            <div>{postItem.post.description}</div>
+            <div className="text-4xl font-semibold leading-tight text-center font-poppins">{postItem.post.title}</div>
+            <div className="text-lg font-normal  py-[2rem] leading-relaxed text-left font-poppins">{postItem.post.description}</div>
             <div className="flex justify-center">
               <Image
                 src={postItem.post.background_image}
@@ -149,81 +156,66 @@ const handleDelete = (id) => {
             </div>
             <div className="flex justify-center items-center">
   <div className="flex items-center w-full">
-    {session?.user?.image ? (
-      <label>
-        <Image
-          src={session.user.image}
-          alt={session.user.email}
-          className="inline ... rounded-full mt-7 mr-2"
-          width={45}
-          height={45}
-        />
-      </label>
-    ) : (
-      <label>
-        <Image
-          src={"https://ionicframework.com/docs/img/demos/avatar.svg"}
-          alt={"default"}
-          className="inline ... rounded-full"
-          width={45}
-          height={45}
-        />
-      </label>
-    )}
-    <div className="flex flex-col w-full">
-      <form onSubmit={()=>handlerSubmit(postItem.post.id)}>
-      <input
-        type="text"
-        placeholder="Id del usuario hasta arreglar..."
-        className="w-full mt-7"
-        name="userId"
-        onChange={handlerChange}
-      />
-      <textarea
-        placeholder="Haz tu comentario..."
-        className=" rounded-[10px] mt-2 w-full h-14"
-        type="text"
-        name="comment"
-        onChange={handlerChange}
-      ></textarea>
-      
-      <button type="submit">Enviar</button>
-      </form>
-      <div>
-        {postItem.post.post_comments?.map((ele)=>{
-         return( 
-          
-         <div>
-          <div>
-          <Image
-          src={ele.user.profile_picture}
-          alt={ele.user.profile_picture}
-          className="inline ... rounded-full"
-          width={45}
-          height={45}
-        />
-         
-          </div>
-          <div>
-            {ele.user.first_name}
-          </div>
-          <div>
-            {ele.user.last_name}
-          </div>
-          <div>
-          {ele.comment}
-          </div>
-         </div>
+    
+     <div className="flex flex-col w-full">
+     <div className="items-center w-[100px]">
+  <div className="text-xs ml-[1rem] block ...">WritenBy</div>
+  <div className="inline pr-2">{postItem.first_name}</div>
+  <div className="inline">{postItem.last_name}</div>
+  </div>
 
-         )
-        })}
+  <div className="flex items-center">
+  <Image
+    src={postItem.profile_picture}
+    alt={postItem.profile_picture}
+    className="rounded-full mb-5 mr-4 "
+    width={45}
+    height={45}
+  />
+  <div className="ml-2 flex-col w-full">
+    <form onSubmit={() => handlerSubmit(postItem.post.id)} className="w-full">
+      <div className="flex">
+        <textarea
+          placeholder="Haz tu comentario..."
+          className="rounded-[10px] mt-2 w-11/12 h-14 border-solid border-4 border-light-blue-500"
+          type="text"
+          name="comment"
+          onChange={handlerChange}
+        ></textarea>
       </div>
-    </div>
+      <div className="mt-2">
+        <button type="submit" className="px-4 py-2 mr-14 bg-orange-500 font-poppins text-white rounded-md">Enviar</button>
+      </div>
+    </form>
   </div>
 </div>
-
+<div>
+  {postItem.post.post_comments?.map((ele) => {
+    return (
+      <div key={ele.id} className="rounded-3xl grid grid-cols-3 gap-2 justify-center mt-[20px] bg-gray-100 px-6 pb-4 pt-4">
+        <div className="col-span-2 flex items-center ">
+          <Image
+            src={ele.user.profile_picture}
+            alt={ele.user.profile_picture}
+            className="inline ... rounded-full"
+            width={45}
+            height={45}
+          />
+          <div className="ml-2">
+            <div className="inline ... pr-2">{ele.user.first_name}</div>
+            <div className="inline ... pr-2">{ele.user.last_name}</div>
           </div>
-        ))}
+        </div>
+        <div className="col-span-3 font-poppins mt-4">{ele.comment}</div>
       </div>
+    );
+  })}
+</div>
+ </div>
+</div>
+</div>
+ </div>
+))}
+</div>
     );
 }
--- components/Blog/PostUser.jsx
+++ components/Blog/PostUser.jsx
@@ -1,29 +1,36 @@
 'use client';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
+import Image from "next/image";
 
 export default function PostUser() {
   const user = useSelector((store) => store);
-
+  const [storeUser, setStoreUser] = useState(null);
   const [formPost, setFormPost] = useState({
     userId: "",
     title: "",
     description: "",
-    background_image: null, // Cambiado a null en lugar de una cadena vacía
+    background_image: null,
   });
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // Verificar que se esté ejecutando en el lado del cliente
+      const userData = localStorage.getItem("userGoogle");
+      setStoreUser(userData ? JSON.parse(userData) : null);
+    }
+  }, []);
 
   const handleChange = (e) => {
     const { name, value, files } = e.target;
 
     if (name === "background_image") {
-      // Si el campo es el input de archivo, almacenar el archivo en el estado
       setFormPost({
         ...formPost,
         [name]: files[0],
       });
     } else {
-      // Para otros campos, actualizar el valor como antes
       setFormPost({
         ...formPost,
         [name]: value,
@@ -33,18 +40,15 @@ export default function PostUser() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+  
     const formData = new FormData();
-    formData.append("userId", formPost.userId);
     formData.append("title", formPost.title);
     formData.append("description", formPost.description);
     formData.append("background_image", formPost.background_image);
-
-    
-
+  
     try {
       const response = await axios.post(
-        `http://localhost:3001/post/${formPost.userId}`,
+        `http://localhost:3001/post/${storeUser?.id}`,
         formData,
         {
           headers: {
@@ -57,47 +61,59 @@ export default function PostUser() {
       console.error(error);
     }
   };
+  
+  
 
   return (
     <div className="my-4">
-  <h3 className="text-lg font-semibold">Crea tu post:</h3>
-  <form onSubmit={handleSubmit} className="mt-4">
-    
-    <div className="mb-4">
-      <label className="block text-sm font-medium">Título:</label>
-      <input
-        type="text"
-        name="title"
-        value={formPost.title}
-        onChange={handleChange}
-        className="px-4 py-2 border border-gray-300 rounded-md w-full"
+     <div style={{ width: "100vw" }}>
+      <Image
+        src={"/image 22.png"}
+        alt={"image_"}
+        className=""
+        
+        width={1920}
+        height={1080}
       />
     </div>
-    <div className="mb-4">
-      <label className="block text-sm font-medium">Descripción:</label>
-      <textarea
-        name="description"
-        value={formPost.description}
-        onChange={handleChange}
-        className="px-4 py-2 border border-gray-300 rounded-md w-full"
-      ></textarea>
+    <div className="w-4/5 bg-gray-100 mx-auto my-auto py-9 mt-6">
+      <form onSubmit={handleSubmit} className="mt-4 ">
+        <div className="mb-4">
+          <label className="block text-sm font-medium">Título:</label>
+          <input
+            type="text"
+            name="title"
+            value={formPost.title}
+            onChange={handleChange}
+            className="px-4 py-2 border border-gray-300 rounded-md w-3/4"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-sm font-medium">Descripción:</label>
+          <textarea
+            name="description"
+            value={formPost.description}
+            onChange={handleChange}
+            className="px-4 py-2 border border-gray-300 rounded-md w-3/4"
+          ></textarea>
+        </div>
+        <div className="mb-4">
+          <label className="block text-sm font-medium">Imagen:</label>
+          <input
+            type="file"
+            name="background_image"
+            onChange={handleChange}
+            className="border border-gray-300 rounded-md w-3/4"
+          />
+        </div>
+        <button
+          type="submit"
+          className="px-4 py-2 bg-orange-500 font-poppins text-white rounded-md"
+        >
+          Publicar
+        </button>
+      </form>
+      </div>
     </div>
-    <div className="mb-4">
-      <label className="block text-sm font-medium">Imagen:</label>
-      <input
-        type="file"
-        name="background_image"
-        onChange={handleChange}
-        className="border border-gray-300 rounded-md w-full"
-      />
-    </div>
-    <button
-      type="submit"
-      className="px-4 py-2 bg-blue-500 text-white rounded-md"
-    >
-      Publicar
-    </button>
-  </form>
-</div>
   );
 }
